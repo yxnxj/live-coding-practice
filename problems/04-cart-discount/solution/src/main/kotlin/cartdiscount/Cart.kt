76a05e8af1ef75b8aefc27fea4c4b0111698521d
yxnxj <micayel1219@gmail.com>
2026-07-11 14:40:26 +0900

    refactor: compose cart discount handlers

--- problems/04-cart-discount/solution/src/main/kotlin/cartdiscount/Cart.kt
+++ problems/04-cart-discount/solution/src/main/kotlin/cartdiscount/Cart.kt
@@ -1,5 +1,7 @@
 package cartdiscount
 
+typealias DiscountHandler = (CheckoutRequest, Int) -> DiscountResult
+
 data class CartItem(
     val name: String,
     val category: Category,
@@ -21,25 +23,118 @@ data class CheckoutResult(
 )
 
 class CheckoutService {
-    fun checkout(request: CheckoutRequest): CheckoutResult {
-        val subtotal = calculateSubtotal(request.items)
+    private fun createDiscountHandler(): List<DiscountHandler>{
+        val handler = mutableListOf<DiscountHandler>()
+        handler.add(::calculateCategoryDiscount)
+        handler.add(::calculateCouponDiscount)
+        handler.add(::calculateMembershipDiscount)
 
-        // 카테고리 할인: 가장 처음 할인 적용
-        val categoryDiscountResult = calculateCategoryDiscount(request.items)
-        var total = subtotal - categoryDiscountResult.totalAmount
-        val discounts: MutableList<Discount> = categoryDiscountResult.discounts.toMutableList()
+        return handler
+    }
 
-        //쿠폰 할인: 카테고리 할인 적용 이후 가격으로 계산
-        request.couponCode?.let {
-            val couponDiscountResult = calculateCouponDiscount(it, total)
-            total -= couponDiscountResult.totalAmount
-            discounts.addAll(couponDiscountResult.discounts)
+    private fun calculateCategoryDiscount(request: CheckoutRequest, total: Int): DiscountResult {
+        val suffixDiscountName = "_CATEGORY"
+        val discounts = mutableListOf<Discount>()
+        var totalAmount = 0
+
+        request.items.forEach { it ->
+            val subtotal = it.unitPrice * it.quantity
+            var name = ""
+            var amount = 0
+
+            when (it.category) {
+                Category.BOOK -> {
+                    amount = (subtotal * 0.1).toInt()
+                    name = "BOOK$suffixDiscountName"
+
+                }
+                Category.FOOD -> {
+                    amount = (subtotal * 0.05).toInt()
+                    name = "FOOD$suffixDiscountName"
+                }
+                else -> { // No discount for OTHER category
+                }
+            }
+
+            if (amount != 0){
+                discounts.add(Discount(name, amount))
+                totalAmount += amount
+            }
         }
 
-        //멤버십 할인: 쿠폰 할인 적용 이후 가격으로 계산
-        val membershipDiscountResult = calculateMembershipDiscount(request.membership, total)
-        total -= membershipDiscountResult.totalAmount
-        discounts.addAll(membershipDiscountResult.discounts)
+        return DiscountResult(
+            discounts = discounts,
+            totalAmount = totalAmount,
+        )
+    }
+
+    private fun calculateCouponDiscount(request: CheckoutRequest, total: Int): DiscountResult {
+        var amount = 0
+
+        when (request.couponCode) {
+            Coupon.WELCOME10.name -> {
+                amount = (total * 0.1).toInt()
+            }
+            Coupon.AMOUNT5000.name -> {
+                if (total >= 50_000) {
+                    amount = 5_000
+                }
+            }
+            null -> {}
+            else -> { throw IllegalArgumentException("Invalid coupon code: $request.couponCode")
+            }
+        }
+
+        return if (amount > 0 && request.couponCode != null) DiscountResult(
+            discounts = listOf(Discount(request.couponCode, amount)),
+            totalAmount = amount,
+        ) else {
+            DiscountResult(
+                discounts = emptyList(),
+                totalAmount = 0,
+            )
+        }
+
+
+    }
+
+    private fun calculateMembershipDiscount(request: CheckoutRequest, total: Int): DiscountResult {
+        var amount = 0
+        val suffixDiscountName = "_MEMBERSHIP"
+
+        when (request.membership) {
+            Membership.VIP -> {
+                amount = (total * 0.03).toInt()
+            }
+
+            else -> { // No discount for NORMAL membership
+            }
+        }
+
+        return if (amount > 0) DiscountResult(
+            discounts = listOf(Discount(request.membership.name + suffixDiscountName, amount)),
+            totalAmount = amount,
+        ) else {
+            DiscountResult(
+                discounts = emptyList(),
+                totalAmount = 0,
+            )
+        }
+    }
+
+    val discountHandlers = createDiscountHandler()
+
+    fun checkout(request: CheckoutRequest): CheckoutResult {
+        val subtotal = calculateSubtotal(request.items)
+        var total = subtotal
+
+        val discounts: MutableList<Discount> = mutableListOf()
+
+        for (handler in discountHandlers) {
+            val discountResult = handler(request, total)
+            total -= discountResult.totalAmount
+            discounts.addAll(discountResult.discounts)
+        }
 
         //배송비 계산 : 할인 적용 이후 가격으로 계산
         val shippingFee = calculateShippingFee(total)
